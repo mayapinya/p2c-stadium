bb7aaa9919814dc1fdee4eea6488394950766fa2
May Apinya <apinya.th28@gmail.com>
2022-10-11 03:41:27 +0700

    update css home

--- src/features/home/CardContent.js
+++ src/features/home/CardContent.js
@@ -4,13 +4,13 @@ import logoAA from '../../assets/logo-text.png';
 function CardContent({ top = '27%', right = '11%', left = '11%' }) {
   return (
     <div className="content-main row d-block" style={{ top, right, left }}>
-      <div className="content-box col-lg-6 col-md-9 col-sm-12 float-end">
+      <div className="content-box col-lg-6 col-md-9 col-sm-12 float-start">
         <img src={logoAA} className="logo-text" alt="text-logo" />
-        <h2 className="text-white content-dec">
+        <h2 className="text-white content-dec text-start">
           สนามบอลหญ้าเทียมขนาด 7 คน มีห้องคลับเฮาน์ติดแอร์ไว้สำหรับผู้ติดตาม
           มีเครื่องดื่มและอาหาร พร้อมบริการ
         </h2>
-        <div className="button-box col-lg-12 col-md-12 col-sm-12 float-end">
+        <div className="button-box col-lg-12 col-md-12 col-sm-12 float-start">
           <button
             type="button"
             className="btn btn-outline-light bt-outline-white bt-main-outline"
